f927ec645fe18d7c485c48124c775c73c29f23bb
ZionLloyd05 <zionlloyd05@gmail.com>
2020-03-27 04:22:14 +0100

    Add project files.

---
+++ SentimentRazor/Api/AnalyzerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using SentimentRazorML.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SentimentRazor.Api
+{
+    [Route("api/[controller]")]
+    public class AnalyzerController : ControllerBase
+    {
+
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public AnalyzerController(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return BadRequest("Neutral");
+
+            var input = new ModelInput { SentimentText = text };
+            var prediction = _predictionEnginePool.Predict(input);
+            var sentiment = Convert.ToBoolean(prediction.Prediction) ? "Toxic" : "Not Toxic";
+
+            return Ok(sentiment);
+        }
+
+    }
+}
---
+++ SentimentRazor/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Microsoft.Extensions.ML;
+using SentimentRazorML.Model;
+
+namespace SentimentRazor
+{
+    public class Startup
+    {
+        private readonly string _modelPath;
+
+        public Startup(IConfiguration configuration)
+        {
+            _modelPath = GetAbsolutPath("MLModel.zip");
+
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddPredictionEnginePool<ModelInput, ModelOutput>()
+                .FromFile(_modelPath);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+
+        public static string GetAbsolutPath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+            return fullPath;
+        }
+    }
+}
